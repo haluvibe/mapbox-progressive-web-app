f2390595e2c0acc47b24ef7e7d0dcea7198142b3
Paul Hayes <water7earth@gmail.com>
2023-03-20 13:54:39 +1100

    test

--- utils/useImageCapture.js
+++ utils/useImageCapture.js
@@ -1,23 +1,36 @@
-export const captureImage = async () => {
+export const useImageCapture = () => {
+    const takePhoto = async () => {
         if ('mediaDevices' in navigator && 'ImageCapture' in window) {
             try {
-                const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-                // The camera stream is now available
-                const imageCapture = new ImageCapture(stream.getVideoTracks()[0]);
-                // Use the imageCapture object to take photos
+              // Get access to the camera
+              const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+              // The camera stream is now available
+              const camera = document.querySelector('#camera');
+              // Set the stream as the source of the camera element
+              camera.srcObject = stream;
+              // Wait for the camera to start playing the stream
+              await camera.play();
         
-                const blob = await imageCapture.takePhoto();
+              // Create an ImageCapture object from the video track of the stream
+              const track = stream.getVideoTracks()[0];
+              const imageCapture = new ImageCapture(track);
+        
+              // Take a photo
+              const blob = await imageCapture.takePhoto();
                 return blob;
-                console.log("🚀 ~ file: useImageCapture.js:10 ~ useImageCapture ~ blob:", blob)
-                // The photo was taken successfully
-                // Use the blob object to display or upload the photo
-        
+              // Use the blob object to display or upload the photo
             } catch (error) {
-                // Handle any errors that occur while accessing the camera or taking the photo
-                console.error('Error:', error);
+              // Handle any errors that occur while accessing the camera
+              console.error('Error accessing camera:', error);
+              return null;
             }
-        } else {
+          } else {
+            // The Image Capture API is not supported
             return null;
-        }
-        return null;
+          }
+    }
+    
+    return {
+        takePhoto
+    };
 }
