b3ac8c7ef9ef9410afaf4254d64a07bce5e325c7
Paul Hayes <water7earth@gmail.com>
2023-03-21 15:31:37 +1100

    adds vehicle details component

---
+++ components/VehicleDetails.tsx
@@ -0,0 +1,68 @@
+import React, { FC, useState } from "react";
+import { Box } from "@mui/material";
+import {
+  ContainedButton,
+  Drawer,
+  Divider,
+  Subtitle2,
+  Body2,
+  Body1,
+  FlexColumn,
+  ArrowBackIcon,
+  FlexRow,
+} from "../src/components/DesignSystem/Library";
+import PrimeMoverVehicleImg from "../assets/images/prime-mover.png";
+import Image from "next/image";
+
+interface Props {}
+
+const VehicleDetails: FC<Props> = ({}: Props): JSX.Element => {
+  return (
+    <>
+      <FlexColumn gap={2}>
+        <Box sx={{ backgroundColor: "black", height: "150px" }} />
+        <ContainedButton>Plan a trip</ContainedButton>
+        <Box sx={{ my: 3 }}>
+          <Divider />
+        </Box>
+        <Subtitle2>Vehicle details</Subtitle2>
+        <FlexColumn>
+          <Body2>Registration number</Body2>
+          <Body1>7MAZ7</Body1>
+        </FlexColumn>
+        <FlexColumn>
+          <Body2>State</Body2>
+          <Body1>QLD</Body1>
+        </FlexColumn>
+        <FlexColumn>
+          <Body2>Class</Body2>
+          <Body1>Class 2 heavy vehicle</Body1>
+        </FlexColumn>
+        <FlexColumn>
+          <Body2>Vehicle</Body2>
+          <Body1>Prime mover</Body1>
+          <Image
+            style={{
+              objectFit: "contain",
+              width: "auto",
+              height: "120px",
+            }}
+            src={PrimeMoverVehicleImg}
+            alt={"Vehicle representing a prime mover"}
+            placeholder={"blur"}
+          />
+        </FlexColumn>
+        <FlexColumn>
+          <Body2>VIN</Body2>
+          <Body1>345234235235DFDF234</Body1>
+        </FlexColumn>
+        <FlexColumn>
+          <Body2>Engine no.</Body2>
+          <Body1>23423523523</Body1>
+        </FlexColumn>
+      </FlexColumn>
+    </>
+  );
+};
+
+export default VehicleDetailsSandbox;
