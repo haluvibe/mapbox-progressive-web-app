60e3b04cdb28d84d1905e89791f634f946c3e832
Paul Hayes <water7earth@gmail.com>
2023-03-23 15:42:43 +1100

    test

--- next.config.js
+++ next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
   dest: 'public',
-  publicExcludes: [
-    '!robots.txt',
-    '!sitemap.xml.gz',
-],
 })
 
 module.exports = withPWA({
   // config
-  dest: 'public',
-  publicExcludes: [
-      '!robots.txt',
-      '!sitemap.xml.gz',
-  ],
+  
 })
